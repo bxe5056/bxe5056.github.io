1c0f06ea76503275fa9613698b5834e115fd3df9
Benjamin Eppinger <bxe5056@gmail.com>
2022-04-25 23:53:29 -0400

    Change 404

--- src/LostPage/LostPage.js
+++ src/LostPage/LostPage.js
@@ -3,11 +3,12 @@ import './LostPage.css';
 
 class LostPage extends React.Component {
   render() {
-      return (
-        <div className="Lost">
-          You're lost
-        </div>
-      );
+    return (
+      <div className='Lost'>
+        This is a test of the auto-generated 404 page. All my contact info is in
+        the footer if you need it.
+      </div>
+    );
   }
 }
 
